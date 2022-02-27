6eb2630ef0c1b2e14777ddd9f0014b841d1a5185
Maykel Santos <maykelsantoz@gmail.com>
2022-02-27 15:15:15 -0300

    Desafio completo.

--- src/index.js
+++ src/index.js
@@ -10,19 +10,87 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "Username not found!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  //
+  const { user } = request;
+
+  //const { username } = request.headers;
+
+  //const user = users.find((user) => user.username === username);
+
+  //console.log(username)
+  //console.log(user.pro)
+
+
+  //console.log(user.todos.length)
+
+
+  if (user.pro) {
+    return next();
+  } else if (!user.pro && user.todos.length + 1 <= 10) {
+    return next();
+  }
+
+  return response.status(403).json({ error: "User must be have a pro plan to keep creating new posts!" });
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+
+  if (!user) {
+    return response.status(404).json({ error: "Username not found!" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Todo not found!" });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  //const { username } = request.headers;
+
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "Username not found!" });
+  }
+
+  request.user = user;
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
@@ -48,6 +116,12 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/users/:id', findUserById, (request, response) => {
+  const { user } = request;
+
+  return response.json(user);
+});
+
+app.get('/users', checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
   return response.json(user);
